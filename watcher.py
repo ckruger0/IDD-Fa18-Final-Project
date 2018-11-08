485a2412f09821c729124e66eafc5f579d13d4dd
Chris Kruger <ckruger@dhcp-nyc-845.redrover.cornell.edu>
2018-11-08 09:57:41 -0500

    watch changer

--- watcher.py
+++ watcher.py
@@ -43,6 +43,7 @@ class Watcher:
                 if input_state == False:
                     lcd.clear()
                     lcd.message("Taking Picture...")
+                    time.sleep(0.2)
                     timestr = time.strftime("%Y%m%d-%H%M%S")
                     camera.capture('images/' + timestr + '.jpg')
                     lcd.clear()
